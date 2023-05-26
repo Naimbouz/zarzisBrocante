483e5228ab6e0f5433f35fa3f9949efc223bd341
Bouzomita Naim <damdoumbouz@gmail.com>
2023-05-26 20:27:51 +0100

    ok

--- src/components/AllNotebooks.vue
+++ src/components/AllNotebooks.vue
@@ -1,5 +1,5 @@
 <template>
-  <listOfProducts :products="getNotebooks"/>
+  <listOfProducts :products="getNotebooks" />
 </template>
 
 <script>
@@ -19,6 +19,6 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
 
+<!-- djheckzgfuezu -->
